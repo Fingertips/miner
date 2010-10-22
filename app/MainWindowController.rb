2ad300ef8330e699f96ed539c41f988f47c78d5a
Eloy Duran <eloy.de.enige@gmail.com>
2010-10-22 11:39:33 +0200

    Expose installGem method to the JS runtime.

--- app/MainWindowController.rb
+++ app/MainWindowController.rb
@@ -3,9 +3,37 @@ class MainWindowController < NSWindowController
     alloc.initWithWindowNibName('MainWindow')
   end
 
+  def self.isSelectorExcludedFromWebScript(selector)
+    selector != :"installGem:"
+  end
+
+  # TODO Returning a pretty name results in a segfault.
+  #def self.webScriptNameForSelector(selector)
+    #'installGem' if selector == :'installGem:'
+  #end
+
   attr_accessor :webView
+
+  def windowWillLoad
+    NSUserDefaults.standardUserDefaults.registerDefaults('WebKitDeveloperExtras' => true)
+
+    # workaround for MacRuby JIT bug
+    respondsToSelector('installGem:')
+  end
 
   def windowDidLoad
     @webView.customUserAgent = 'MinerApp'
+    @webView.frameLoadDelegate = self
+
+    request = NSURLRequest.requestWithURL(NSBundle.mainBundle.URLForResource('index', withExtension:'html'))
+    @webView.mainFrame.loadRequest(request)
+  end
+
+  def webView(webView, didFinishLoadForFrame:frame)
+    webView.windowScriptObject.setValue(self, forKey:'MinerApp')
+  end
+
+  def installGem(name)
+    puts "Install gem: #{name}"
   end
 end
