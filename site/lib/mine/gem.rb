e86832e9e1aada2d12d91b9658f5cc0a23fd8271
Manfred Stienstra <manfred@fngtps.com>
2010-10-22 15:47:02 +0200

    Improve the scoring a little bit.

--- site/lib/mine/gem.rb
+++ site/lib/mine/gem.rb
@@ -1,7 +1,8 @@
 module Mine
   class Gem
-    DISQUALIFIED = 0.0
-    USED_TOO_MUCH = 3000
+    DISQUALIFIED    = 0.0
+    USED_TOO_MUCH   = 3000
+    USED_TOO_LITTLE = 200
     
     include Attributes
     
@@ -14,6 +15,8 @@ module Mine
          @attributes['downloads'].zero? or
          @attributes['downloads'] > USED_TOO_MUCH
         DISQUALIFIED
+      elsif @attributes['downloads'] < USED_TOO_LITTLE
+        @attributes['version_downloads'].to_f / 100
       else
         @attributes['version_downloads'].to_f / @attributes['downloads']
       end
--- site/test/lib/mine/gem_test.rb
+++ site/test/lib/mine/gem_test.rb
@@ -4,7 +4,9 @@ describe Mine::Gem do
   it "computes a score based on the number of downloads" do
     [
       [{'downloads' => 0, 'version_downloads' => 0}, Mine::Gem::DISQUALIFIED],
-      [{'downloads' => 100, 'version_downloads' => 20},  0.2],
+      [{'downloads' => 100, 'version_downloads' => 20}, 0.2],
+      [{'downloads' => 2, 'version_downloads' => 2}, 0.02],
+      [{'downloads' => 15835, 'version_downloads' => 15835}, Mine::Gem::DISQUALIFIED],
       [{'downloads' => Mine::Gem::USED_TOO_MUCH + 1, 'version_downloads' => 100}, Mine::Gem::DISQUALIFIED]
     ].each do |attributes, expected|
       Mine::Gem.new(attributes).score.should == expected
