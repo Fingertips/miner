4bb7bfab049410008cf37d4df56310f380297be2
Manfred Stienstra <manfred@fngtps.com>
2010-10-22 14:06:18 +0200

    Hit Rubyforge instead of stubbing the results.

--- site/app/controllers/gems_controller.rb
+++ site/app/controllers/gems_controller.rb
@@ -1,13 +1,5 @@
 class GemsController < ApplicationController
   def index
-    @gems = gems
-  end
-  
-  private
-  
-  def gems
-    JSON.parse(File.read(Rails.root + 'test/fixtures/files/gems.json')).map do |description|
-      Mine::Gem.new(description)
-    end
+    @gems = Mine.all
   end
 end
