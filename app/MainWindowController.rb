9eeb8d55611005dd1ca0337a121f53ff61b07c42
Eloy Duran <eloy.de.enige@gmail.com>
2010-10-22 15:52:41 +0200

    Cleanup and remove useless menu items.

--- app/MainWindowController.rb
+++ app/MainWindowController.rb
@@ -1,4 +1,6 @@
 class MainWindowController < NSWindowController
+  URL = NSURL.URLWithString('http://minerapp.com')
+
   def self.controller
     alloc.initWithWindowNibName('MainWindow')
   end
@@ -6,11 +8,6 @@ class MainWindowController < NSWindowController
   def self.isSelectorExcludedFromWebScript(selector)
     !%w{ installGem: isGemInstalled:version: }.include?(selector.to_s)
   end
-
-  # TODO MacRuby bug: Returning a pretty name results in a segfault.
-  #def self.webScriptNameForSelector(selector)
-    #'installGem' if selector == :'installGem:'
-  #end
 
   attr_accessor :webView
 
@@ -26,22 +23,13 @@ class MainWindowController < NSWindowController
     @webView.customUserAgent = 'MinerApp'
     @webView.frameLoadDelegate = self
     @webView.windowScriptObject.setValue(self, forKey:'MinerApp')
-    
-    request = NSURLRequest.requestWithURL(NSURL.URLWithString('http://miner.local'))
-    @webView.mainFrame.loadRequest(request)
+    @webView.mainFrame.loadRequest(NSURLRequest.requestWithURL(URL))
   end
 
   def isGemInstalled(name, version:version)
-    # TODO RubyGems (on osx) is so broken, it does not search in the home folder, so using the API is pointless...
-    #dep = Gem::Dependency.new(name, version)
-    #!Gem.source_index.search(dep).empty?
-
     File.exist?(File.join(userGemPath, "gems", "#{name}-#{version}"))
   end
 
-  # TODO Just using $?.success? isn't really enough, because RubyGems can
-  # install a gem and still return that it wasn't a success. For example,
-  # if YARD fails to generate docs.
   def installGem(name)
     Dispatch::Queue.concurrent(:default).async do
       NSLog("Install gem `#{name}' to gem path `#{userGemPath}'")
